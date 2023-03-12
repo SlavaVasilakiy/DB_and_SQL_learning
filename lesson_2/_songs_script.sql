9a70871e9b67270ed60f4071d248fdc482b9de95
SlavaVasilakiy <slk55slk@gmail.com>
2023-03-12 19:38:26 +0300

    Homework 2

--- lesson_2/_songs_script.sql
+++ lesson_2/_songs_script.sql
@@ -10,12 +10,19 @@ CREATE TABLE song_name (
 
 DROP TABLE IF EXISTS song_data;
 CREATE TABLE song_data (
-	song_data_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+	id_data BIGINT UNSIGNED NOT NULL PRIMARY KEY,
 	song_artist VARCHAR(100) NOT NULL,
 	song_album VARCHAR(100) NOT NULL,
 	
-	INDEX idx_song_artist_album(song_artist, song_album)
+	INDEX idx_song_data(id_data)
 ) COMMENT 'song_data';
 
-ALTER TABLE song_data ADD CONSTRAINT songs_data_id
-FOREIGN KEY (song_data_id) REFERENCES song_name(id);
+ALTER TABLE song_data ADD CONSTRAINT id_data
+FOREIGN KEY (id_data) REFERENCES song_name(id);
+
+ALTER TABLE song_data ADD COLUMN song_bitrate INT;
+ALTER TABLE song_data ADD COLUMN song_date INT;
+ALTER TABLE song_data MODIFY COLUMN song_date DATE;
+ALTER TABLE song_data ADD COLUMN song_added_date DATETIME DEFAULT NOW();
+
+
